6ac1b471df84ff23ebc9e419cfed3a8188dbd254
fatimahaider786 <aimanfatima888786@gmail.com>
2026-09-13 14:31:49 +0500

    cors origin change

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 // CORS configuration (yeh khud options/preflight handle kar leta hai)
 app.use(cors({
-  origin: "*", 
+  origin: ["https://full-stack-frontent.vercel.app", "http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
